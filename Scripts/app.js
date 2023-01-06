19e0ddeb4ab39a3f0662ebbc9e6ed1045223c0c4
Ulissesortega <Ulisses.ortega@hotmail.com>
2023-01-06 14:09:37 -0800

    update

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,8 @@
 let wickedCss = ["floater", "barrelRoll", "rollerRight", "rollerLeft", "heartBeat", "pulse", "rotation",
-    "sideToSide", "zoomer", "zoomOut", "spinner", "wiggle", "shake", "pound", "slideUp", "slideDown",
-    "slideRight", "slideLeft", "fadeIn", "fadeOut", "rotateInRight", "rotateInLeft", "rotateIn", "bounceIn"];
+  "sideToSide", "zoomer", "zoomOut", "spinner", "wiggle", "shake", "pound", "slideUp", "slideDown",
+  "slideRight", "slideLeft", "fadeIn", "fadeOut", "rotateInRight", "rotateInLeft", "rotateIn", "bounceIn"];
 
-let btn2 = document.getElementById('btn2');    
+let btn2 = document.getElementById('btn2');
 let changed = true;
 let cssSelected = Math.floor(Math.random() * wickedCss.length)
 let btn3 = document.getElementById('btn3');
@@ -16,69 +16,68 @@ var audio = document.getElementById('audio');
 var playPauseBTN = document.getElementById('playPauseBTN');
 var count = 0;
 
-function myAlert(){
-alert("Welcome to JS Object Dom Manipulation");
-let userName = prompt("Please Enter your name");
-if (userName == null || userName == "") {
-  txt = "No Name Provided";
-}else{
-  txt = "Hello, " + userName + "!";
-}
-alert(txt);
+function myAlert() {
+  alert("Welcome to JS Object Dom Manipulation");
+  let userName = prompt("Please Enter your name");
+  if (userName == null || userName == "") {
+    txt = "No Name Provided";
+  } else {
+    txt = "Hello, " + userName + "!";
+  }
+  alert(txt);
 }
 
-btn2.addEventListener('click', () =>{
-    if (changed) {
+btn2.addEventListener('click', () => {
+  if (changed) {
     test1.innerText = "I have now been changed \n Click Try me 1 more time";
     test1.className = "Change1";
     changed = false;
-  } else{    
-    location.reload();    
-  } 
+  } else {
+    location.reload();
+  }
 })
 
 
-btn3.addEventListener('click', () =>{
-  location.reload();    
+btn3.addEventListener('click', () => {
+  location.reload();
 })
 
 
-btnOne.addEventListener("click", function(){  
+btnOne.addEventListener("click", function () {
   btnOne.innerText = "I have now been changed";
 })
 
-btnTwo.addEventListener("click", function(){
+btnTwo.addEventListener("click", function () {
   btnTwo.innerText = "This is change with a class";
   btnTwo.className = "fantasyChange"
 })
 
-btnThree.addEventListener("mouseover", function(){
-  btnThree.className = "change2" 
+btnThree.addEventListener("mouseover", function () {
+  btnThree.className = "change2"
 })
 
-btnFour.addEventListener("mouseover", function(){
-  bodybg.className = "body1" 
-
+btnFour.addEventListener("click", function () {
+  btnFour.innerText = "Tron Legacy \n https://ww7.0123movie.net/movie/tron-legacy-2556.html \n Click the play button once \n close the window that will open \n click play again :)";
 })
 
 
-function playPause(){
-	if(count == 0){
-		count = 1;
-		audio.play();
-		playPauseBTN.innerHTML = "Pause ⏸";
-	}else{
-		count = 0;
-		audio.pause();
-		playPauseBTN.innerHTML = "Play ►";
-	}
-
+function playPause() {
+  if (count == 0) {
+    count = 1;
+    audio.play();
+    playPauseBTN.innerHTML = "Pause ⏸";
+  } else {
+    count = 0;
+    audio.pause();
+    playPauseBTN.innerHTML = "Play ►";
+  }
 }
 
-function stop(){
-	playPause()
-	audio.pause();
-	audio.currentTime = 0;
-	playPauseBTN.innerHTML = "Play ►";
+function stop() {
+  playPause()
+  audio.pause();
+  audio.currentTime = 0;
+  playPauseBTN.innerHTML = "Play ►";
 }
+
 
